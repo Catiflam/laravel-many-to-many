ac5d5e860d1e75a566256ff99380c4f2fb64ad7d
Catiflam <gilfrid1@gmail.com>
2023-10-27 15:01:15 +0200

    first commit

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Type;
+
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $_types = [
+            "Definizione",
+            "Pianificazione",
+            "Esecuzione",
+            "Controllo",
+          ];
+      
+          foreach ($_types as $_type) {
+            $type = new Type();
+            $type->label = $_type;
+            $type->color = $faker->hexColor();
+            $type->save();
+          }
+    }
+}
